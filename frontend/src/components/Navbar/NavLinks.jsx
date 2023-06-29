c06add7391d894ead4cc6dc59f412a759545e623
mrprotocoll <lekanvgbg@gmail.com>
2023-06-29 15:11:22 +0100

    split the login and logout feature into session component

--- frontend/src/components/Navbar/NavLinks.jsx
+++ frontend/src/components/Navbar/NavLinks.jsx
@@ -1,12 +1,8 @@
-import { useEffect, useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { destroySession, TOKENKEY } from 'util/auth';
 import NavLinkItem from './NavLinkItem';
 
 // eslint-disable-next-line react/prop-types
 const NavLinks = ({ toggleNavbar }) => {
-  const navigate = useNavigate();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
   const items = [
     { name: 'TOURS', link: '/' },
     { name: 'MY RESERVATIONS', link: '/reservations' },
@@ -14,19 +10,6 @@ const NavLinks = ({ toggleNavbar }) => {
     { name: 'ADD RESERVATION', link: '/add-reservation' },
     { name: 'DELETE TOUR', link: '/delete-tour' },
   ];
-
-  const handleLogout = (e) => {
-    e.preventDefault();
-    setIsLoggedIn(false);
-    destroySession();
-    navigate('/login');
-  };
-
-  useEffect(() => {
-    if (localStorage.getItem(TOKENKEY)) {
-      setIsLoggedIn(true);
-    }
-  }, [isLoggedIn, handleLogout]);
 
   return (
     <ul className="mt-4 w-full text-lg sm:text-lg font-bold" onClick={toggleNavbar}>
@@ -36,18 +19,7 @@ const NavLinks = ({ toggleNavbar }) => {
         ))
       }
 
-      {isLoggedIn ? (
-        <li className="pl-4 py-2 hover:bg-primary hover:text-white ">
-          <button type="button" onClick={handleLogout}>
-            LOGOUT
-          </button>
-        </li>
-      ) : (
-        <li className="pl-4 py-2 hover:bg-primary hover:text-white sm:text-lg">
-          <NavLink to="/login">LOGIN</NavLink>
-        </li>
-
-      )}
+      
 
     </ul>
   );
