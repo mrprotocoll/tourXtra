c3da03c5e97246476f31d2d7713f3a6c5db9f2cf
mrprotocoll <lekanvgbg@gmail.com>
2023-07-04 21:06:03 +0100

    Add restore button to delete tour

--- frontend/src/components/tour/DeleteTourCard.jsx
+++ frontend/src/components/tour/DeleteTourCard.jsx
@@ -1,44 +1,58 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-const DeleteTourCard = ({tour}) => {
-    return (
-        <div
-            key={tour.id}
-            className="flex md:flex-row md:w-full w-[310px]  flex-col items-center justify-between m-auto shadow-md rounded-lg mb-4 md:m-5 md:h-[150px] px-[10px]"
-            >
-            <div className="md:w-[30%] md:h-[150px] p-2">
-                <img
-                src={process.env.REACT_APP_API_ROOT + tour.image_url}
-                alt={tour.name}
-                className="w-full h-full"
-                />
-            </div>
-            <h2 className="mb-4 text-lg font-semibold text-slate-800">
-                {tour.name}
-            </h2>
+const DeleteTourCard = ({ tour, handleDelete, handleRestore }) => (
+  <div
+    className="flex md:flex-row md:w-full w-[310px]  flex-col items-center justify-between m-auto shadow-md rounded-lg mb-4 md:m-5 md:h-[150px] px-[10px]"
+  >
+    <div className="md:w-[30%] md:h-[150px] p-2">
+      <img
+        src={process.env.REACT_APP_API_ROOT + tour.image_url}
+        alt={tour.name}
+        className="w-full h-full"
+      />
+    </div>
+    <h2 className="mb-4 text-lg font-semibold text-slate-800">
+      {tour.name}
+    </h2>
 
-            <div className="flex flex-col justify-center mb-[1rem]">
-                {tour.status && <span className="bg-primary text-white py-1 px-3 rounded-3xl">Removed</span>}
-                {!tour.status && (
-                <button
-                type="button"
-                className="bg-[red] text-[#fff] px-4 py-2 rounded-lg"
-                onClick={() => handleDelete(tour.id)}
-                >
-                Delete
-                </button>
-                )}
-            </div>
-        </div>
-    )
-}
+    <div className="flex flex-col justify-center mb-[1rem]">
+      {tour.status
+      && (
+        <>
+          <button
+            type="button"
+            className="bg-black mb-3 text-[#fff] px-4 py-2 rounded-3xl"
+            onClick={() => handleRestore(tour.id)}
+          >
+            Restore
+          </button>
+          <span className="bg-primary text-white py-1 px-3 rounded-3xl">Removed</span>
+        </>
+
+      )}
+
+      {!tour.status && (
+      <button
+        type="button"
+        className="bg-[red] text-[#fff] px-4 py-2 rounded-3xl"
+        onClick={() => handleDelete(tour.id)}
+      >
+        Delete
+      </button>
+      )}
+    </div>
+  </div>
+);
 
 DeleteTourCard.propTypes = {
-    tour: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        image_url: PropTypes.string.isRequired,
-        status: PropTypes.bool.isRequired,
-    })
-}
+  tour: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image_url: PropTypes.string.isRequired,
+    status: PropTypes.bool.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+  handleDelete: PropTypes.func.isRequired,
+  handleRestore: PropTypes.func.isRequired,
+};
 
 export default DeleteTourCard;
--- frontend/src/pages/DeleteTourPage.jsx
+++ frontend/src/pages/DeleteTourPage.jsx
@@ -1,3 +1,4 @@
+import DeleteTourCard from 'components/tour/DeleteTourCard';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -33,34 +34,7 @@ const DeleteTourPage = () => {
 
           { myTours.length > 0
             ? myTours.map((info) => (
-              <div
-                key={info.id}
-                className="flex md:flex-row md:w-full w-[310px]  flex-col items-center justify-between m-auto shadow-md rounded-lg mb-4 md:m-5 md:h-[150px] px-[10px]"
-              >
-                <div className="md:w-[30%] md:h-[150px] p-2">
-                  <img
-                    src={`http://localhost:3000${info.image_url}`}
-                    alt={info.name}
-                    className="w-full h-full"
-                  />
-                </div>
-                <h2 className="mb-4 text-lg font-semibold text-slate-800">
-                  {info.name}
-                </h2>
-
-                <div className="flex flex-col justify-center mb-[1rem]">
-                  {info.status && <span className="bg-primary text-white py-1 px-3 rounded-3xl">Removed</span>}
-                  {!info.status && (
-                  <button
-                    type="button"
-                    className="bg-[red] text-[#fff] px-4 py-2 rounded-lg"
-                    onClick={() => handleDelete(info.id)}
-                  >
-                    Delete
-                  </button>
-                  )}
-                </div>
-              </div>
+              <DeleteTourCard key={info.id} tour={info} handleDelete={handleDelete} />
             ))
             : (
               <p className="text-lg font-bold text-gray-800 text-center">
